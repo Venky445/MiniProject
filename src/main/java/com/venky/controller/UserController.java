0f0fbb3cd6cea8d5b44130595931f2bd79686635
venky <venkatraogurijala3@gmail.com>
2023-08-04 20:58:25 +0530

    creating html pages and controller methods

--- src/main/java/com/venky/controller/UserController.java
+++ src/main/java/com/venky/controller/UserController.java
@@ -1,5 +1,22 @@
 package com.venky.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+	@GetMapping("/login")
+	public String loginPage() {
+		return "login";	}
+	@GetMapping("/signup")
+	public String signUpPage() {
+		return "signup";	}
+	@GetMapping("/forgot")
+	public String forgotPwdPage() {
+		return "forgotPwd";	}
+	@GetMapping("/unlock")
+	public String unlockPage() {
+		return "unlock";	}
+
 
 }
